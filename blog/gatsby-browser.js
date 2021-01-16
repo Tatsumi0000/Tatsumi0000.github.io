e281867e0dfeec27fb3235a04bbb6e653bc05a8e
Tatsumi0000 <ti18027@student.miyazaki-u.ac.jp>
2021-01-17 06:42:50 +0900

    Add

--- blog/gatsby-browser.js
+++ blog/gatsby-browser.js
@@ -3,12 +3,11 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 // normalize CSS across browsers
 import "./src/styles/normalize.css"
-import './src/utils/tailwind.css'
 // custom CSS styles
 import "./src/styles/style.css"
-
 // Highlighting for code blocks
 import "prismjs/themes/prism-okaidia.css"
 import "prismjs/plugins/line-numbers/prism-line-numbers.css"
 // https://littlebylittle.work/2020/01/gatsby-syntax-highlighting/
 import "./src/styles/code-highlight.css"
+import './src/utils/tailwind.css'
