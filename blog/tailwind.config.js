0b89b3208fc967edb800d3c738c60a4fe9bf2e30
Tatsumi0000 <datsunote0211@gmail.com>
2021-01-19 02:06:45 +0900

    Fix: fontFamilyを指定

--- blog/tailwind.config.js
+++ blog/tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
   purge: false,
   darkMode: false,
   theme: {
-    extend: {},
+    fontFamily: {
+      body: [
+        'Avenir',
+        'Helvetica Neue',
+        'Helvetica',
+        'Arial',
+        'Hiragino Sans',
+        'ヒラギノ角ゴシック',
+        'メイリオ',
+        'Meiryo',
+        'YuGothic',
+        'Yu Gothic',
+        'ＭＳ Ｐゴシック',
+        'MS PGothic',
+        'sans-serif'
+      ]
+    }
   },
   variants: {
     extend: {},
