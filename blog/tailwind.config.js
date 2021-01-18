65cb619f9f09e63897ccdc0f6a130e54ea32e6ee
Tatsumi0000 <datsunote0211@gmail.com>
2021-01-18 23:48:04 +0900

    Add

---
+++ blog/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: false,
+  darkMode: false,
+  fontFamily: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    fontFamily: false,
+    listStyleType: false,
+  }
+}
