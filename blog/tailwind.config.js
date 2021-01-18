57ba12da00127fd3bbe9d5ac1d0253bb0d22a681
Tatsumi0000 <datsunote0211@gmail.com>
2021-01-19 02:12:02 +0900

    Add: PurgeCSSの設定を入れた

--- blog/tailwind.config.js
+++ blog/tailwind.config.js
@@ -1,25 +1,7 @@
 module.exports = {
   purge: false,
   darkMode: false,
-  theme: {
-    fontFamily: {
-      body: [
-        'Avenir',
-        'Helvetica Neue',
-        'Helvetica',
-        'Arial',
-        'Hiragino Sans',
-        'ヒラギノ角ゴシック',
-        'メイリオ',
-        'Meiryo',
-        'YuGothic',
-        'Yu Gothic',
-        'ＭＳ Ｐゴシック',
-        'MS PGothic',
-        'sans-serif'
-      ]
-    }
-  },
+  theme: {},
   variants: {
     extend: {},
   },
