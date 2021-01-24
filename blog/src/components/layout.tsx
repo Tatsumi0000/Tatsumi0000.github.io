07bfacc1d7b9e2b984880b8d64b9a7c3c36214d9
Tatsumi0000 <datsunote0211@gmail.com>
2021-01-25 00:24:09 +0900

    Change: ヘッダーのデザインをどこのURLでも一緒にした

--- blog/src/components/layout.tsx
+++ blog/src/components/layout.tsx
@@ -5,21 +5,11 @@ import Bio from "../components/bio"
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
+  const header = (
+    <h1 className="main-heading">
+      <Link to="/">{title}</Link>
+    </h1>
+  )
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
